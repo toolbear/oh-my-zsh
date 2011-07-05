c03473826b4250069bc36dbf714f973cf638c829
Tim Taylor <tim@tool-man.org>
2011-07-05 01:47:55 -0400

    better 'less' defaults

---
+++ toolbear/less.zsh
@@ -0,0 +1,10 @@
+export LESS="\
+    --hilite-unread \
+    --no-init \
+    --no-lessopen \
+    --QUIT-AT-EOF \
+    --quit-if-one-screen \
+    --quit-on-intr \
+    --RAW-CONTROL-CHARS \
+    --tabs=4 \
+"
