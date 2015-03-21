25bb71c8bef898f5bdbf6e30a70fa5fa34e3b86e
Tim Taylor <tim@tool-man.org>
2015-03-21 12:14:15 -0700

    emacs: better 'e' shortcut

--- toolbear/emacs.zsh
+++ toolbear/emacs.zsh
@@ -1,3 +1,21 @@
 export EDITOR=emacsclient
 
-alias e="$EDITOR -n"
+if [[ ${EDITOR} == emacs ]] ; then
+  EDITOR=emacsclient
+  function e () {
+    local -aU opts
+    opts+=-n
+    opts+=(${argv[1,-1]})
+
+    for opt in ${opts} ; do
+      case ${opt} in
+      -nw|-t|--tty)
+        if [[ ${INSIDE_EMACS} ]] ; then
+	  printf "stubbornly refusing to open nested emacs frame\n" >/dev/stderr
+	  return 1
+	fi
+      esac
+    done
+    ${EDITOR} ${opts[@]}
+  }
+fi
