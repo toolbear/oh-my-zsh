8bac8696a6157422e220e9b155304ad9bfe603b3
Tim Taylor <tim@tool-man.org>
2015-05-12 18:51:39 -0700

    emacs: smart EDITOR & emacsclient with `M-x shell|term`

--- toolbear/emacs.zsh
+++ toolbear/emacs.zsh
@@ -1,21 +1,24 @@
-export EDITOR=${EDITOR:-emacs}
-
-if [[ ${EDITOR} == emacs ]] ; then
-  EDITOR=emacsclient
-  function e () {
-    local -aU opts
-    opts+=-n
-    opts+=(${argv[1,-1]})
-
-    for opt in ${opts} ; do
-      case ${opt} in
-      -nw|-t|--tty)
-        if [[ ${INSIDE_EMACS} ]] ; then
-	  printf "stubbornly refusing to open nested emacs frame\n" >/dev/stderr
-	  return 1
-	fi
-      esac
-    done
-    ${EDITOR} ${opts[@]}
-  }
-fi
+case ${EDITOR} in
+  emacs|emacsclient*)
+    if [ ${INSIDE_EMACS} ] ; then
+      export EMACS_SOCKET=${EMACS_SOCKET:-$(basename $(lsof -U -a -p $(ps -p $$ -o ppid=) |awk '$1 == "Emacs" { print $8; exit; }'))}
+    fi
+    export EDITOR="emacsclient ${EMACS_SOCKET+-s ${EMACS_SOCKET}}"
+    function e () {
+      local -aU opts
+      opts+=-n
+      opts+=(${argv[1,-1]})
+      if [ ${INSIDE_EMACS} ] ; then
+        opts+=-s
+        opts+=${EMACS_SOCKET}
+        for opt in ${opts} ; do
+          case ${opt} in
+            -nw|-t|--tty)
+              printf "stubbornly refusing to open nested emacs frame\n" >/dev/stderr
+              return 1
+          esac
+        done
+      fi
+      emacsclient ${opts[@]}
+    }
+esac
